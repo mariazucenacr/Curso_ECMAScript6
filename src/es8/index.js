55da93be2f5b747782827f8d1d886a63e73dd0ae
Mariazucena Calderón Riera <mariazucenacalderonriera@gmail.com>
2022-10-08 15:07:24 -0500

    Se realizo un ejemplo de async-await y un ejercicio para entender mejor su funcionamiento en es8

--- src/es8/index.js
+++ src/es8/index.js
@@ -49,3 +49,59 @@ console.log('food'.padEnd(12,' ------'));
 const data_1 ={
     fronend: 'Azucena',
 }
+
+//*ASYNC AWAIT
+const helloWorld = () =>{
+    return new Promise((resolve,reject =>{
+        (true)
+            ? setTimeout(() => resolve('Hello World!'))
+            : reject (new Error('Test Error'))
+    }))
+};
+//con la promesa ya hecha vamos a ver como funciona
+//async await
+const helloAsync = async () => {
+    const hello = await helloWorld();
+    console.log(hello);
+};
+helloAsync();
+
+//como ejecutar correctamente el async await
+const anotherFunction = async () => {
+    //se usa para manejar errores se usa try-catch
+    try{
+        const hello = await helloWorld();
+        console.log(hello);
+    }catch{
+        console.log(error);
+    }
+};
+anotherFunction();
+//----------------------------------------------------------------------------------------------------------------------
+//ejercicio de ejemplo
+// Async Await
+const prepareIceCream = (money) => {
+    return new Promise((resolve, reject) => {
+        if(money >= 500) {    
+            console.log('We are preparing your Ice cream...')
+            setTimeout(() => {
+                resolve('Here you are a Chocolate Ice cream');
+            }, 6000);  //3. Si el dinero que ingresaste es mayor o igual a 500, entonces te preparamos el helado. El proceso toma 6 segundos.
+        } else {
+            reject('Sorry! You do not have enough money :('); //3. Si el dinero es menor a 500, lo sentimos, no te alcanza
+        }
+    });
+}
+
+const buyIceCream = async (money) => { 
+    try{
+        console.log('Welcome to Ice Cream World!');
+        const result = await prepareIceCream(money); //2. Esperas aquí mientras envíamos el dinero, lo verifican y te preparan el helado 
+        console.log(result); //4. Imprimimos tu pedido! si todo salió bien.
+        console.log('Thanks for your purchase!');
+    } catch(e) {
+        console.log(e); //4. Te decimos que no salió bien el proceso porque tu dinero no es suficiente.
+    }
+}
+
+buyIceCream(600); //1. llamas a la funcion comprar helado e Ingresas el dinero
