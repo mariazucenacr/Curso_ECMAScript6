52e2167c120f843cc46dfde2546b195cd4dedf3f
Mariazucena Calderón Riera <mariazucenacalderonriera@gmail.com>
2022-10-08 12:34:24 -0500

    En index.js se creo un ejemplo para explicar como funciona en padding en ES8

--- src/es8/index.js
+++ src/es8/index.js
@@ -24,3 +24,21 @@ const values = Object.values(data_);
 console.log(values);
 //también podemos contar cuantos elementos tenia
 console.log(values.length);
+
+//*-PADDING
+//*Rellenar un string
+const string = 'Hello';
+//como podemos agregar una cadena añadida a este string, antes de
+console.log(string.padStart(7,'hi'));//entre los parentesis se pone cuantos carateres vamos a poner
+                                //considerar que por ejemplo se quiere añadir 2 caracteres entonces
+                                //se cuanta cuantos tiene la palabra y se le añade los dos que se 
+                                //quiere por eso son 7 y al lado se pone lo que se quiere añadir 
+                                //en esos 2 caracteres agregados en este caso es hi
+
+//como podemos agregar una cadena añadida a este string, después de
+console.log(string.padEnd(12,' -----'));//esto de igual forma se cuentan los caracteres de la palabra
+                                        //y los que se quieren añadir y se agrega al final 
+
+//Este padding sirve para mostrar una estructura de elementos, por la parte del front
+//por ejemplo en la impresión de un producto
+console.log('food'.padEnd(12,' ------'));
