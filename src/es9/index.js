ac8242f55f4d9d16008b7727575dbb7768646337
Mariazucena Calderón Riera <mariazucenacalderonriera@gmail.com>
2022-10-10 15:35:16 -0500

    Se agrego al archivo index.js un ejemplo de como se usa la propiedad de propagación para anidar dos objetos

--- src/es9/index.js
+++ src/es9/index.js
@@ -1,7 +1,7 @@
 //*Operador de REPOSO(Rest)
 const obj ={
     name : 'Azucena',
-    age: 32,
+    age: 26,
     country: 'Perú'
 };
 
@@ -14,3 +14,21 @@ console.log(name,all);
 //del objeto por ejemplo quiero la edad entonces seria asi
 let {country, ...all} = obj; 
 console.log(all);//y asi aparece lo que necesito
+
+//*Utilizando las propiedades de PROPAGACIÓN
+//Donde vamos a unir uno,dos,tres o más elementos de objetos
+//a un nuevo objeto
+const obj_0 = {
+    nombre: 'Azucena',
+    age: 26
+};
+
+const obj_1 ={
+    //nosotros queremos la propiedad de propagación
+    //para anidar estos dos objetos entonces
+    //simplemente añadimos lo siguiente
+    ...obj_0,
+    country: 'Peru'
+};
+
+console.log(obj_1);
