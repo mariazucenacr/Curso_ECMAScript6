06d563aa1e2f334c75ea849f386b504ae712fad6
Mariazucena Calderón Riera <mariazucenacalderonriera@gmail.com>
2022-10-12 17:43:10 -0500

    Se añadio a index.js otro metodo de aplanamiento de array, flatMap

--- src/es10/index.js
+++ src/es10/index.js
@@ -1,4 +1,4 @@
-//*-Aplanamiento de ARRAYS
+//*-Aplanamiento de ARRAYS (FLAT)
 //creamos un arreglo con ciertos niveles en el array principal
 let array = [1,2,3,[1,2,3,[1,2,3]]];
 //para devolver una matriz con cualquier matriz aplanada
@@ -10,3 +10,9 @@ console.log(array.flat(2));//pasariamos 2 como argumento
 //de esta forma vemos como ha aplanado el array
 //podemos pasarle el valor segun sea el caso de la matriz o de
 //los elementos que va a tener
+
+//*-Aplanamiento de ARRAYS (FLAT-MAP)
+//mapear cada elemento, luego pasarle una función
+//y aplanar el resultado
+let array_1=[1,2,3,4,5];
+console.log(array_1.flatMap(value => [value, value *2]));
