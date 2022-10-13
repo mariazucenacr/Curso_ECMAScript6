504e30f347356a31e4f06f5226b43fe01e3dba86
Mariazucena Calderón Riera <mariazucenacalderonriera@gmail.com>
2022-10-13 13:05:33 -0500

    Se realizo una pequeña codificación en index.js para dar el ejemplo de import, conjuntamente con los otros archivos creados

--- src/es11/index.js
+++ src/es11/index.js
@@ -2,3 +2,7 @@
 //traer o llamar algun archivo dentro de otra pieza de código
 //cuando se desencadene un llamado, en este caso un click
 const button = document.getElementById("btn");
+button.addEventListener("click",async function(){
+    const module = await import("./file.js");
+    module.hello();
+});
