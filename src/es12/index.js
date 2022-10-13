4214b953c1c0fb95ed4ed301bb566de27757a024
Mariazucena Calderón Riera <mariazucenacalderonriera@gmail.com>
2022-10-13 15:07:09 -0500

    Se añadio un ejemplo de clase privada para su entender como funciona7

--- src/es12/index.js
+++ src/es12/index.js
@@ -8,3 +8,31 @@ console.log(replaceString);
 //Ahora con el nuevo replace all
 const replaceString2 = string.replaceAll("Javascript","Phyton");
 console.log(replaceString2);
+
+//*-Métodos de clases privadas
+class Message {
+    show(val){
+        console.log(val);
+    };
+};
+
+const message = new Message();
+message.show('Hola');
+//como volver de forma privada este recurso se pone el simbolo #
+//para dar la representación de que ahora el método el privado y
+//solo sera accedido dentro de la clase
+class Message {
+    #show(val){//<----
+        console.log(val);
+    };
+    //de la misma forma con get y set
+    get #add(val){
+        //....
+    };
+};
+
+const message = new Message();
+message.show('Hola');
+//con ello nos va a salir un error, porque no esta disponible;por el
+//hecho de que ahora es privado
+
