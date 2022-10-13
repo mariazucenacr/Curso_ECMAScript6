46bc61b19fbaa47f7ea89b5d882ee4604a264722
Mariazucena Calderón Riera <mariazucenacalderonriera@gmail.com>
2022-10-13 12:59:01 -0500

    Se creo otro archivo file.js donde habra una función que servira para el llamado

--- src/es11/index.js
+++ src/es11/index.js
@@ -1,3 +1,4 @@
 //*-Dinamic import
 //traer o llamar algun archivo dentro de otra pieza de código
 //cuando se desencadene un llamado, en este caso un click
+const button = document.getElementById("btn");
